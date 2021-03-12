dad2a8cf10fb4ccf85b0093d66389f869c0e2695
Estefania <tetillamas@gmail.com>
2021-03-11 23:16:47 -0600

    fondo

--- ProyectoFerreteria/Interfaz/pantalla_inicio.py
+++ ProyectoFerreteria/Interfaz/pantalla_inicio.py
@@ -7,29 +7,36 @@ class Pantalla_de_inicio:
 
     def __init__(self):
         self.raiz = Tk()
-        self.raiz.geometry('1000x700')
-        self.raiz.config(bg="#3C5CE4")
+        self.raiz.geometry('1169x563')
         self.raiz.title("Inicio")
-        self.frame = Frame(self.raiz)
-        self.frame.pack()
+        self.raiz.resizable(False, False)
+        self.imagen = PhotoImage(file="fondo.GIF")
+        self.fondo = Label(self.raiz, image=self.imagen).place(x=0, y=0)
+        #self.frame = Frame(self.raiz)
+       # self.frame.pack(fill="both", expand=1)
         self.ventana_principal()
         self.raiz.mainloop()
 
     def ventana_principal(self):
-        Boton_provedores = Button(self.frame, text="Provedores", width="50", bg="black", fg="white" )
-        Boton_provedores.grid(row=2, column=0)
+        """ Muestra los botones de las distintas pestañas que tenemos,asi como el nombre de la ferreteria"""
+        Imagen_provedores = Image.open(file="Ventas.GIF")
+        Boton_provedores = Button(self.raiz, image=Imagen_provedores, width=20)
+        Boton_provedores.place(x=20, y=35)
 
-        Boton_productos = Button(self.frame, text="Productos", width="50", bg="black", fg="white" )
-        Boton_productos.grid(row=3, column=0)
+        Boton_productos = Button(self.raiz, text="Productos", width=20, height=1, relief="ridge", borderwidth=5, bg="black", fg="white", font="Arial" )
+        Boton_productos.place(x=20, y=135)
 
-        Boton_inventario = Button(self.frame, text="Inventario", width="50", bg="black", fg="white" )
-        Boton_inventario.grid(row=4, column=0)
+        Boton_inventario = Button(self.raiz, text="Inventario", width=20, height=1, relief="ridge", borderwidth=5, bg="black", fg="white", font="Arial")
+        Boton_inventario.place(x=20, y=235)
 
-        Boton_ventas = Button(self.frame, text="Ventas", width="50", bg="black", fg="white" )
-        Boton_ventas.grid(row=5, column=0)
+        Boton_ventas = Button(self.raiz, text="Ventas", width=20, height=1, relief="ridge", borderwidth=5, bg="black", fg="white", font="Arial" )
+        Boton_ventas.place(x=20, y=335)
 
-        Boton_admin = Button(self.frame, text="Administrador", width="50", bg="black", fg="white" )
-        Boton_admin.grid(row=6, column=0)
+        Boton_admin = Button(self.raiz, text="Administrador", width=20, height=1, relief="ridge", borderwidth=5, bg="black", fg="white", font="Arial" )
+        Boton_admin.place(x=20, y=435)
+        
+
+       
 
 
 uno = Pantalla_de_inicio()
