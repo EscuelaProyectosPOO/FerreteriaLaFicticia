0e198e05ce398667b40354fac34c01e93b938cbf
Estefania <tetillamas@gmail.com>
2021-03-12 08:39:29 -0600

    Pantalla dde inicio con la decoracion en los bbotones bien hecha

--- Interfaz/pantalla_inicio.py
+++ Interfaz/pantalla_inicio.py
@@ -13,34 +13,36 @@ class Pantalla_de_inicio:
         self.raiz.resizable(False, False)
         self.imagen = PhotoImage(file="fondo.GIF")
         self.fondo = Label(self.raiz, image=self.imagen).place(x=0, y=0)
-        #self.frame = Frame(self.raiz)
-       # self.frame.pack(fill="both", expand=1)
         self.ventana_principal()
         self.raiz.mainloop()
 
     def ventana_principal(self):
         """ Muestra los botones de las distintas pestañas que tenemos,asi como el nombre de la ferreteria"""
-        #imagen_provedores = PhotoImage(file="fondo.GIF").subsample(2,2)
-        Boton_provedores = Button(self.raiz, bg="#FFFF00", text="Provedor",font="Arial", cursor="hand2", border=0 , width=20, command=lambda:instaciaciones())
-        Boton_provedores.place(x=20, y=35)
+        self.imagen_provedores = PhotoImage(file="Provedores.GIF")
+        self.Boton_provedores = Button(self.raiz, image=self.imagen_provedores, width=136, height=15, cursor="hand2", border=0, command=lambda:instaciaciones())
+        self.Boton_provedores.place(x=40, y=50)
 
-        Boton_productos = Button(self.raiz, bg="#FFFF00", text="Productos",font="Arial", cursor="hand2", border=0 , width=20,  command=lambda:instaciaciones() )
-        Boton_productos.place(x=20, y=135)
+        self.imagen_productos = PhotoImage(file="Productos.GIF")
+        self.Boton_productos = Button(self.raiz, image=self.imagen_productos, width=121, height=17,cursor="hand2",border=0,  command=lambda:instaciaciones() )
+        self.Boton_productos.place(x=45, y=150)
 
-        Boton_inventario = Button(self.raiz, bg="#FFFF00", text="Inventario",font="Arial", cursor="hand2", border=0 , width=20,  command=lambda:instaciaciones())
-        Boton_inventario.place(x=20, y=235)
+        self.imagen_inventario = PhotoImage(file="Inventario.GIF")
+        self.Boton_inventario = Button(self.raiz, image=self.imagen_inventario, width=121, height=20,cursor="hand2",border=0,  command=lambda:instaciaciones() )
+        self.Boton_inventario.place(x=55, y=250)
 
-        Boton_ventas = Button(self.raiz, bg="#FFFF00", text="Ventas",font="Arial", cursor="hand2", border=0 , width=20, command=lambda:instaciaciones() )
-        Boton_ventas.place(x=20, y=335)
+        self.imagen_ventas = PhotoImage(file="Ventas.GIF")
+        self.Boton_ventas = Button(self.raiz, image=self.imagen_ventas, width=88, height=24,cursor="hand2",border=0,  command=lambda:instaciaciones() )
+        self.Boton_ventas.place(x=60, y=345)
 
-        Boton_admin = Button(self.raiz, bg="#FFFF00", text="Administrador",font="Arial", cursor="hand2", border=0 , width=20, command=lambda:instaciaciones())
-        Boton_admin.place(x=20, y=435)
+        self.imagen_admin = PhotoImage(file="Administrador.GIF")
+        self.Boton_admin = Button(self.raiz, image=self.imagen_admin, width=169, height=26,cursor="hand2",border=0,  command=lambda:instaciaciones() )
+        self.Boton_admin.place(x=33, y=443)
 
         def instaciaciones():
             uno = Pantalla_administrador()
         
 
-    
+
 
 
 uno = Pantalla_de_inicio()
