a0888f92897d2b9b8d5dddaf1baa78409b2d9b81
MaFerLlamas <fernandaavilallamas@gmail.com>
2021-03-16 08:53:01 -0600

    --ya autentifica

--- Interfaz/login.py
+++ Interfaz/login.py
@@ -1,55 +1,59 @@
 # -*- coding: utf-8 -*-
 # #!/usr/bin/env python
-
+from pantalla_inicio import Pantalla_de_inicio
 from Tkinter import *
-
 
 class Usuarios:
     def __init__(self):
+        self.raiz = Tk()
+        self.raiz.title('Inicio de sesión')
+        self.raiz.geometry('250x200')
+        #self.raiz.config(bg='#f0ab3f')
+        self.raiz.resizable(False, False)
+        self.ventana()
 
-        raiz = Tk()
-        raiz.title('Inicio de sesión')
-        raiz.geometry('250x200')
-        raiz.config(bg='#f0ab3f')
-        raiz.resizable(False, False)
-        espacio = Label(raiz)
-        espacio.grid(row=1)
-        espacio.config(bg='#f0ab3f')
+        self.raiz.mainloop()
 
-        nombretext = StringVar()
-        nombre = nombretext.get()
-        print nombre
-
-        nl = Label(raiz, text='Usuario:', font=18)
-        nl.grid(row=2, column=0, padx=10, pady=5, sticky='e')
-        nl.config(bg='#f0ab3f')
-        tn = Entry(raiz, textvariable=nombretext)
-        tn.grid(row=2, column=1)
-
-        cl = Label(raiz, text='Contraseña', font=18)
-        cl.grid(row=3, column=0, sticky='e', padx=10, pady=5)
-        cl.config(bg='#f0ab3f')
-        tc = Entry(raiz)
-        tc.grid(row=3, column=1)
-        tc.config(show='*')
-
-        be = Button(raiz, text='Entrar', font=18, command=comprueba(nombre))
-        be.grid(row=4, column=0, pady=20)
-        be.config(justify=LEFT, bg='#172caa')
-
-        br = Button(raiz, text='Registrar usuario', font=18)
-        br.grid(row=4, column=1, pady=20)
-        br.config(justify=LEFT, bg='#172caa')
-        raiz.mainloop()
+    def ventana(self):
 
 
-def comprueba(nom):
-    if nom == 'Ana':
-        print 'Hola'
-    elif nom == '':
-        print 'vacío'
-    else:
-        print 'Nombre incorrecto'
+        self.espacio = Label(self.raiz)
+        self.espacio.grid(row=1)
+        #self.espacio.config(bg='#f0ab3f')
+
+        self.nombretext = StringVar()
+        self.nombre = self.nombretext.get()
+        #print self.nombre
+
+        self.nl = Label(self.raiz, text='Usuario:', font=18)
+        self.nl.grid(row=2, column=0, padx=10, pady=5, sticky='e')
+        #self.nl.config(bg='#f0ab3f')
+        self.tn = Entry(self.raiz, textvariable=self.nombretext)
+        self.tn.grid(row=2, column=1)
+
+        self.cl = Label(self.raiz, text='Contraseña', font=18)
+        self.cl.grid(row=3, column=0, sticky='e', padx=10, pady=5)
+        self.tc = Entry(self.raiz)
+        self.tc.grid(row=3, column=1)
+        self.tc.config(show='*')
+
+        def comprueba():
+            print '...'
+            nom = self.nombretext.get()
+            if nom == 'Ana':
+                print 'Bienvenida'
+                uno = Pantalla_de_inicio()
+            elif nom == '' or nom ==' ':
+                print 'vacío'
+            else:
+                print 'nombre incorrecto'
+        self.be = Button(self.raiz, text='Entrar', font=18, command=lambda:comprueba())
+        self.be.grid(row=4, column=0, pady=20)
+
+        self.br = Button(self.raiz, text='Registrar usuario', font=18)
+        self.br.grid(row=4, column=1, pady=20)
+
+
 
 
 Inicio = Usuarios()
