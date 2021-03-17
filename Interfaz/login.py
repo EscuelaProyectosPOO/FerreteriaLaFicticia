68f2ae03b9640c7cd9a2a6c52ad5959d91aa7c1d
Estefania <tetillamas@gmail.com>
2021-03-17 09:01:01 -0600

    imagenes

--- Interfaz/login.py
+++ Interfaz/login.py
@@ -37,16 +37,6 @@ class Usuarios:
         self.tc.grid(row=3, column=1)
         self.tc.config(show='*')
 
-         def comprueba():
-            print '...'
-            nom = self.nombretext.get()
-            if nom == 'Ana':
-                print 'Bienvenida'
-                uno = Pantalla_de_inicio().ventana_principal()
-            elif nom == '' or nom ==' ':
-                print 'vacío'
-            else:
-                print 'nombre incorrecto'
         
         self.be = Button(self.raiz, text='Entrar', font=18, command=lambda:comprueba())
         self.be.grid(row=4, column=0, pady=20)
@@ -54,6 +44,16 @@ class Usuarios:
         self.br = Button(self.raiz, text='Registrar usuario', font=18)
         self.br.grid(row=4, column=1, pady=20)
 
+    def comprueba(self):
+            print '...'
+            self.nom = self.nombretext.get()
+            if nom == 'Ana':
+                print 'Bienvenida'
+                uno = Pantalla_de_inicio().ventana_principal()
+            elif nom == '' or nom ==' ':
+                print 'vacío'
+            else:
+                print 'nombre incorrecto'
        
 
 
