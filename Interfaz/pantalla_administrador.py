6558f6e0fa951d1a35dd058bb49bbb62f19681e5
Estefania <tetillamas@gmail.com>
2021-03-21 19:39:01 -0600

    carcasa ventas, administrador

--- Interfaz/pantalla_administrador.py
+++ Interfaz/pantalla_administrador.py
@@ -3,14 +3,8 @@
 
 from Tkinter import *
 
-class Pantalla_administrador:
+class Administrador():
 
-    def __init__(self):
-        self.raiz = Tk()
-        self.raiz.geometry('1169x563')
-        self.raiz.title("tk")
-        self.raiz.resizable(False, False)
-        #self.frame = Frame(self.raiz)
-       # self.frame.pack(fill="both", expand=1)
-        #self.raiz.mainloop()
+    def Registrar_usuario(self):
+        pass
 
---
+++ Interfaz/pantalla_ventas.py
@@ -0,0 +1,13 @@
+
+class Ventas():
+    def __init__(self, Total_de_ventas, Productos_vendidos_divididos_por_tipos, Dinero_recolectado):
+        self.Total_de_ventas = Total_de_ventas
+        self.Productos_vendidos_divididos_por_tipos = Productos_vendidos_divididos_por_tipos
+        self.Dinero_recolectado = Dinero_recolectado
+
+    def Registrar_venta(self):
+        pass
+    def Modificar_venta(self):
+        pass
+    def Reportes_ventas(self):
+        pass
