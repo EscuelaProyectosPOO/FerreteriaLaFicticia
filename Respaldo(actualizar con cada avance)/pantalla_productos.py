3ace1f7d89197ddf012206a0762a42948feb89ee
MaFerLlamas <fernandaavilallamas@gmail.com>
2021-03-21 19:25:38 -0600

    carcasa lista

---
+++ Respaldo(actualizar con cada avance)/pantalla_productos.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class productos:
+    def __init__(self, Nombre_producto, Precio, Codigo_producto, Cantidad_existencia, Marca_producto, Modelo_producto):
+        self.nombre=Nombre_producto
+        self.precio=Precio
+        self.codigo=Codigo_producto
+        self.cantidad=Cantidad_existencia
+        self.marca=Marca_producto
+        self.modelo=Modelo_producto
+
+    def Registrar_productos(self):
+        pass
+
+    def Borrar_productos(self):
+        pass
+
+    def Modificar(self):
+        pass
+
+    def Reporte(self):
+        pass
